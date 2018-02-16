73ff48562cdad559df37478d7bd2fd30939d62ef
Kamil Khadeev <camelot729@gmail.com>
2018-02-16 19:29:42 +0300

    Yandex Translator

--- bot/modes/YAT.py
+++ bot/modes/YAT.py
@@ -3,7 +3,7 @@ translate = YandexTranslate('trnsl.1.1.20180216T133517Z.9eb599729cc6f038.adb96dd
 
 class YandexTranslator:
 
-    def __init__(self, input_text, output_lang, input_lang = ''):
+    def __init__(self, input_text, output_lang = 'en', input_lang = ''):
         if input_lang == '':
             self.lang = '' + output_lang + ''
         else:
@@ -20,9 +20,9 @@ class YandexTranslator:
 if __name__ == '__main__':
     input_text = 'привет?'
     # input_lang = 'ru'
-    output_lang = 'en'
+    # output_lang = 'en'
     
-    yad = YandexTranslator(input_text, output_lang)
+    yad = YandexTranslator(input_text)
     # print(yad.lang, yad.text)
     print(yad.transl(yad.text, yad.lang))
     # print(yad.detect_lang(yad.text))
